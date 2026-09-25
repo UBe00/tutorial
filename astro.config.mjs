70cd7a0616d6d9eb8bba74afcc2ea7e34362d877
Jehu Uribe <a23100139@ceti.mx>
2026-09-25 17:34:31 -0600

    Agrego isla de Preact y modo oscuro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-	site: "https://soft-smakager-d59252.netlify.app",
+  site: "https://soft-smakager-d59252.netlify.app",
+  integrations: [preact()],
 });
