c186ad3e6b2cd69abddae1a9d15941bc54b6d264
Jehu Uribe <a23100139@ceti.mx>
2026-09-25 17:19:10 -0600

    Agrego feed RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: "https://soft-smakager-d59252.netlify.app",
+});
